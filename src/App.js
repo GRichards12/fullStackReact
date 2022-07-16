797dc6bf6a826044f995add48a24eeda2b423c09
Gwyl Richards <98527262+GRichards12@users.noreply.github.com>
2022-07-16 16:54:31 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
-import { SignIn } from "./components/signIn";
+import { SignUp } from "./components/signUp";
 import "./App.css";
+import { SignIn } from "./components/SignIn";
 
 const App = () => {
   const [photos, setPhotos] = useState([]);
@@ -8,7 +9,7 @@ const App = () => {
   const [user, setUser] = useState({username:"Signed out", email:"undefined", password:"undefined" });
 
   const fetchImages = async () => {
-    const response = await fetch("https://picsum.photos/v2/list");
+    const response = await fetch("https://picsum.photos/v2/list?limit=10");
     const data = await response.json();
     setPhotos(data);
   };
@@ -19,18 +20,20 @@ const App = () => {
 
   return (
     <div className="App">
-      <SignIn setUser={setUser}/>
-      <p>username:{user.username}</p>
-      <button onClick={()=>setSignedIn(!signedIn)}>{!signedIn?<span>Sign in</span>:<span>Sign out</span>}</button>
+      <SignUp setUser={setUser} setSignedIn={setSignedIn}/>
+      <SignIn setUser={setUser} setSignedIn={setSignedIn}/>
+      <p>Username:{user.username}</p>
+      {/* <button onClick={()=>setSignedIn(!signedIn)}>{!signedIn?<span>Sign in</span>:<span>Sign out</span>}</button> */}
       {signedIn?
-      photos.map((item, index) => {
+      photos.map((item) => {
         return (
-          <div>
-            <img alt= "best image you've ever seen" src={item.download_url} key={index}/>
+          <div key={item.id}>
+            <img alt= "best thing you've ever seen" src={item.download_url} key={item.id}/>
             <h2>{item.author}</h2>
           </div>
         );
-      }) :<span>please sign in</span>}
+      }) :<span>Please sign in to view your feed!</span>}
+      {signedIn===true&&<button onClick={()=>window.location.reload()}>Sign out</button>}
       </div>
   )}
 export default App;
