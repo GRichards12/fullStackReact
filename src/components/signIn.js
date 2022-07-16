00e47a243c6439f74d8f62a5ef57859b0e7f3bb4
Gwyl Richards <98527262+GRichards12@users.noreply.github.com>
2022-07-16 16:52:32 +0100

    Update signIn.js

--- src/components/signIn.js
+++ src/components/signIn.js
@@ -1,23 +1,25 @@
 import { useState } from "react";
-import { signUp } from "../utils/signUp";
+import { signIn } from "../utils";
 
-export const SignIn = ({ setUser }) => {
+export const SignIn = ({ setUser, setSignedIn }) => {
   const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const submitSign = async (event) => {
+  const submitSignIn = async (event) => {
     event.preventDefault();
-    await signUp(username, email, password, setUser);
+    // setUser(username,email,password);
+    await signIn(username, password, setUser, setSignedIn);
   };
 
   return (
-    <form onSubmit={submitSign}>
+    <form onSubmit={submitSignIn}>
+        <h1>Login:</h1>
+        <p>Username:</p>
       <input onChange={(event) => setUsername(event.target.value)} />
-      <input onChange={(event) => setEmail(event.target.value)} />
+        <p>Password:</p>
       <input onChange={(event) => setPassword(event.target.value)} />
+      <br></br>
       <button type="submit">Submit</button>
     </form>
   );
 };
-
